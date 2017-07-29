1b16fb12f2b3903d966f86070d36bc40a88f8be7
iory <ab.ioryz@gmail.com>
2017-07-30 01:46:51 +0900

    Add shell mode

--- euslisp-mode.el
+++ euslisp-mode.el
@@ -25,6 +25,30 @@
 
 (load (concatenate #'string euslisp-mode-source-dir "euslisp-find-definition.el"))
 
+
+(defvar euslisp-shell--parent-buffer nil)
+(defvar euslisp-shell--interpreter)
+(defvar euslisp-shell--interpreter-args)
+(defvar euslisp-shell--prompt-calculated-input-regexp nil
+  "Calculated input prompt regexp for inferior euslisp shell.
+Do not set this variable directly, instead use
+`euslisp-shell-prompt-set-calculated-regexps'.")
+
+(defvar euslisp-shell--prompt-calculated-output-regexp nil
+  "Calculated output prompt regexp for inferior euslisp shell.
+Do not set this variable directly, instead use
+`euslisp-shell-set-prompt-regexp'.")
+
+(defcustom euslisp-shell-interpreter "irteus"
+  "Default Euslisp interpreter for shell."
+  :type 'string
+  :group 'euslisp)
+
+(defcustom euslisp-shell-interpreter-args ""
+  "Default arguments for the Euslisp interpreter."
+  :type 'string
+  :group 'euslisp)
+
 ;;; Mode Definition  ==========================================================
 
 (defun euslisp-show-version ()
@@ -96,6 +120,254 @@
   (list (concat "(" (regexp-opt '("setq") t) "\\>") '(1 font-lock-type-face nil t))
   )
  )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Shell ====================================================================
+
+
+(defun euslisp-shell-get-or-create-process ()
+  "Get or create an inferior Euslisp process for current buffer and return it."
+  (let* ((bufname (format "*%s*" (euslisp-shell-get-process-name nil)))
+         (dedbufname (format "*%s*" (euslisp-shell-get-process-name t)))
+         (proc (get-buffer-process bufname))
+         (dedproc (get-buffer-process dedbufname)))
+    (if euslisp-dedicated-shells
+        (if dedproc
+            dedproc
+          (run-euslisp (euslisp-shell-parse-command) t)
+          (get-buffer-process dedbufname))
+      (if dedproc
+          dedproc
+        (if proc
+            proc
+          (run-euslisp (euslisp-shell-parse-command))
+          (get-buffer-process bufname))))))
+
+
+(when (not (fboundp 'euslisp-shell-get-process-name))
+  (defun euslisp-shell-get-process-name (dedicated)
+    "Default shell name."
+    "Euslisp"))
+
+
+(defmacro euslisp-shell-with-environment (&rest body)
+  "Modify shell environment during execution of BODY.
+Temporarily sets `process-environment' and `exec-path' during
+execution of body.  If `default-directory' points to a remote
+machine then modifies `tramp-remote-process-environment' and
+`euslisp-shell-remote-exec-path' instead."
+  (declare (indent 0) (debug (body)))
+  (let ((vec (make-symbol "vec")))
+    `(progn
+       (let* ((,vec
+               (when (file-remote-p default-directory)
+                 (ignore-errors
+                   (tramp-dissect-file-name default-directory 'noexpand))))
+              (process-environment
+               (if ,vec
+                   process-environment
+                 (euslisp-shell-calculate-process-environment)))
+              (exec-path
+               (if ,vec
+                   exec-path
+                 (euslisp-shell-calculate-exec-path)))
+              (tramp-remote-process-environment
+               (if ,vec
+                   (euslisp-shell-calculate-process-environment)
+                 tramp-remote-process-environment)))
+         (when (tramp-get-connection-process ,vec)
+           ;; For already existing connections, the new exec path must
+           ;; be re-set, otherwise it won't take effect.  One example
+           ;; of such case is when remote dir-locals are read and
+           ;; *then* subprocesses are triggered within the same
+           ;; connection.
+           (euslisp-shell-tramp-refresh-remote-path
+            ,vec (euslisp-shell-calculate-exec-path))
+           ;; The `tramp-remote-process-environment' variable is only
+           ;; effective when the started process is an interactive
+           ;; shell, otherwise (like in the case of processes started
+           ;; with `process-file') the environment is not changed.
+           ;; This makes environment modifications effective
+           ;; unconditionally.
+           (euslisp-shell-tramp-refresh-process-environment
+            ,vec tramp-remote-process-environment))
+         ,(macroexp-progn body)))))
+
+
+(defun euslisp-shell-make-comint (cmd proc-name &optional show internal)
+  "Create a Euslisp shell comint buffer."
+  (save-excursion
+    (euslisp-shell-with-environment
+      (let* ((proc-buffer-name
+              (format (if (not internal) "*%s*" " *%s*") proc-name)))
+        (when (not (comint-check-proc proc-buffer-name))
+          (let* ((cmdlist (split-string-and-unquote cmd))
+                 (interpreter (car cmdlist))
+                 (args (cdr cmdlist))
+                 (buffer (apply #'make-comint-in-buffer proc-name proc-buffer-name
+                                interpreter nil args))
+                 (euslisp-shell--parent-buffer (current-buffer))
+                 (process (get-buffer-process buffer))
+                 (euslisp-shell--interpreter interpreter)
+                 (euslisp-shell--interpreter-args
+                  (mapconcat #'identity args " ")))
+            (with-current-buffer buffer
+              (inferior-euslisp-mode))
+            (when show (display-buffer buffer))
+            (and internal (set-process-query-on-exit-flag process nil))))
+        proc-buffer-name))))
+
+
+(defun run-euslisp (&optional cmd dedicated show)
+  "Run an inferior Euslisp process."
+  (interactive
+   (if current-prefix-arg
+       (list
+        (read-shell-command "Run Euslisp: " (euslisp-shell-calculate-command))
+        (y-or-n-p "Make dedicated process? ")
+        (= (prefix-numeric-value current-prefix-arg) 4))
+     (list (euslisp-shell-calculate-command) nil t)))
+  (get-buffer-process
+   (euslisp-shell-make-comint
+    (or cmd (euslisp-shell-calculate-command))
+    (euslisp-shell-get-process-name dedicated) show)))
+
+
+(defun euslisp-shell-get-buffer ()
+  "Return inferior Euslisp buffer for current buffer.
+If current buffer is in `inferior-euslisp-mode', return it."
+  (if (derived-mode-p 'inferior-euslisp-mode)
+      (current-buffer)
+    (let* ((dedicated-proc-name (euslisp-shell-get-process-name t))
+           (dedicated-proc-buffer-name (format "*%s*" dedicated-proc-name))
+           (global-proc-name  (euslisp-shell-get-process-name nil))
+           (global-proc-buffer-name (format "*%s*" global-proc-name))
+           (dedicated-running (comint-check-proc dedicated-proc-buffer-name))
+           (global-running (comint-check-proc global-proc-buffer-name)))
+      ;; Always prefer dedicated
+      (or (and dedicated-running dedicated-proc-buffer-name)
+          (and global-running global-proc-buffer-name)))))
+
+
+(defun euslisp-shell-get-process ()
+  "Return inferior Euslisp process for current buffer."
+  (get-buffer-process (euslisp-shell-get-buffer)))
+
+
+(defun euslisp-shell-get-process-or-error (&optional interactivep)
+  "Return inferior Euslisp process for current buffer or signal error.
+When argument INTERACTIVEP is non-nil, use `user-error' instead
+of `error' with a user-friendly message."
+  (or (euslisp-shell-get-process)
+      (if interactivep
+          (user-error
+           "Start a Euslisp process first with `M-x run-euslisp' or `%s'."
+           ;; Get the binding.
+           (key-description
+            (where-is-internal
+             #'run-euslisp overriding-local-map t)))
+        (error
+         "No inferior Euslisp process running."))))
+
+
+(defun euslisp-shell-send-string (string &optional process msg)
+  "Send STRING to inferior Euslisp PROCESS.
+When optional argument MSG is non-nil, forces display of a
+user-friendly message if there's no process running; defaults to
+t when called interactively."
+  (interactive
+   (list (read-string "Euslisp command: ") nil t))
+  (let ((process (or process (euslisp-shell-get-process-or-error msg))))
+    (when (string-match ".\n+." string) ;; Multiline.
+        (setq string (replace-regexp-in-string "\n" " " string)))
+    (comint-send-string process string)
+    (when (or (not (string-match "\n\\'" string))
+              (string-match "\n[ \t].*\n?\\'" string))
+      (comint-send-string process "\n"))))
+
+
+(defun euslisp-shell-calculate-command ()
+  "Calculate the string used to execute the inferior Euslisp process."
+  (format "%s %s"
+          (shell-quote-argument euslisp-shell-interpreter)
+          euslisp-shell-interpreter-args))
+
+(when (not (fboundp 'euslisp-shell-calculate-process-environment))
+  (defun euslisp-shell-calculate-process-environment ()
+    "Compatibility function for older Emacsen."
+    process-environment))
+
+(when (not (fboundp 'euslisp-shell-calculate-exec-path))
+  (defun euslisp-shell-calculate-exec-path ()
+    "Compatibility function for older Emacsen."
+    exec-path))
+
+(defun euslisp-shell-tramp-refresh-remote-path (vec paths)
+  "Update VEC's remote-path giving PATHS priority."
+  (let ((remote-path (tramp-get-connection-property vec "remote-path" nil)))
+    (when remote-path
+      (euslisp-shell--add-to-path-with-priority remote-path paths)
+      (tramp-set-connection-property vec "remote-path" remote-path)
+      (tramp-set-remote-path vec))))
+
+(defun euslisp-shell-tramp-refresh-process-environment (vec env)
+  "Update VEC's process environment with ENV."
+  ;; Stolen from `tramp-open-connection-setup-interactive-shell'.
+  (let ((env (append (when (fboundp #'tramp-get-remote-locale)
+                       ;; Emacs<24.4 compat.
+                       (list (tramp-get-remote-locale vec)))
+         (copy-sequence env)))
+        (tramp-end-of-heredoc
+         (if (boundp 'tramp-end-of-heredoc)
+             tramp-end-of-heredoc
+           (md5 tramp-end-of-output)))
+  unset vars item)
+    (while env
+      (setq item (tramp-compat-split-string (car env) "="))
+      (setcdr item (mapconcat 'identity (cdr item) "="))
+      (if (and (stringp (cdr item)) (not (string-equal (cdr item) "")))
+    (push (format "%s %s" (car item) (cdr item)) vars)
+  (push (car item) unset))
+      (setq env (cdr env)))
+    (when vars
+      (tramp-send-command
+       vec
+       (format "while read var val; do export $var=$val; done <<'%s'\n%s\n%s"
+         tramp-end-of-heredoc
+         (mapconcat 'identity vars "\n")
+         tramp-end-of-heredoc)
+       t))
+    (when unset
+      (tramp-send-command
+       vec (format "unset %s" (mapconcat 'identity unset " ")) t))))
+
+(defun euslisp-util-clone-local-variables (from-buffer &optional regexp)
+  "Clone local variables from FROM-BUFFER.
+Optional argument REGEXP selects variables to clone and defaults
+to \"^euslisp-\"."
+  (mapc
+   (lambda (pair)
+     (and (symbolp (car pair))
+          (string-match (or regexp "^euslisp-")
+                        (symbol-name (car pair)))
+          (set (make-local-variable (car pair))
+               (cdr pair))))
+   (buffer-local-variables from-buffer)))
+
+(define-derived-mode inferior-euslisp-mode comint-mode "Inferior Euslisp"
+  (when euslisp-shell--parent-buffer
+    (euslisp-util-clone-local-variables euslisp-shell--parent-buffer))
+  (compilation-shell-minor-mode 1))
+
+(defun euslisp-send-current-statement ()
+  (interactive)
+  (let ((selection (buffer-substring-no-properties (point-at-bol) (point-at-eol))))
+    (euslisp-shell-send-string selection)))
+
+(defun euslisp-shell-send-region (from to)
+  (interactive "r")
+  (let ((selection (buffer-substring-no-properties from to)))
+    (euslisp-shell-send-string selection)))
 
 
 (provide 'euslisp-mode)
