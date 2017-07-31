b76ce2291673a04f46066bd44a5f667035a01df0
iory <ab.ioryz@gmail.com>
2017-07-31 21:27:36 +0900

    Release 0.0.6

--- euslisp-mode.el
+++ euslisp-mode.el
@@ -3,7 +3,7 @@
 ;; Author: iory <ab.ioryz@gmail.com>
 ;; Maintainer: iory <ab.ioryz@gmail.com>
 ;; Created: April 13, 2016
-;; Version: 0.0.5
+;; Version: 0.0.6
 ;; Keywords: Euslisp, euslisp, GitHub
 ;; URL: https://github.com/iory/euslisp-mode
 ;; Package-Requires: ((emacs "23") (s "1.9") (exec-path-from-shell "0"))
@@ -15,7 +15,7 @@
 
 ;;; Constants =================================================================
 
-(defconst euslisp-mode-version "0.0.5"
+(defconst euslisp-mode-version "0.0.6"
   "Euslisp mode version number.")
 
 (defconst euslisp-output-buffer-name "*euslisp-output*"
