486a8a428f8f4f11918ca4c919790b8b89c99eaa
iory <ab.ioryz@gmail.com>
2017-08-31 04:06:19 +0900

    Add license to GPLv3+

--- README.md
+++ README.md
@@ -18,3 +18,7 @@ If you want to set catkin source path, please execute '''(euslisp-change-env)'''
 (euslisp-change-env "/opt/ros/indigo")
 (euslisp-change-env "/PATH/TO/CATKIN_WORKSPACE")
 ```
+
+# License
+
+Released under the terms of [GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html) or (at your option) any later version. See [LICENSE.txt](https://github.com/iory/euslisp-mode/blob/master/LICENSE.txt).
--- euslisp-mode.el
+++ euslisp-mode.el
@@ -8,6 +8,19 @@
 ;; URL: https://github.com/iory/euslisp-mode
 ;; Package-Requires: ((emacs "23") (s "1.9") (exec-path-from-shell "0") (helm-ag 0.58))
 
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (require 's)
 (require 'exec-path-from-shell)
 (require 'helm-ag)
